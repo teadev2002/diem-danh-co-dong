c62623ffe73e564b5c09b6fae6e663836c24f35d
teadev2002 <anhhoanggg855@gmail.com>
2024-02-26 14:14:12 +0700

    tkb

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "Trung Anh", "Thế  Anh", "Thanh Phong", "Hữu Tú", "Công Nam", "Minh Đăng",
         "Mai Thắng", "Mai Lợi", "Hữu Danh", "Thanh Hậu", "Minh Tú",
         "Quang Thiệu", "Hải Đăng", "Tấn Lộc", "Đức Chung", "Thiện Nghĩa", "Minh Hiếu", "Tuấn Khải",
-         "Quốc Thái", "Hoàng Phúc", "Công Tài", "Trung Hiếu"
+         "Quốc Thái", "Hoàng Phúc", "Trung Hiếu"
     ];
 
     console.log(sampleData);
