97288789221618f197cd7b02ce6573f6789650da
teadev2002 <anhhoanggg855@gmail.com>
2024-03-27 20:56:22 +0700

    name

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "Trung Anh", "Thế  Anh", "Thanh Phong", "Hữu Tú", "Công Nam", "Minh Đăng",
         "Mai Thắng", "Mai Lợi", "Thanh Hậu", "Minh Tú",
          "Hải Đăng", "Tấn Lộc", "Đức Chung", "Thiện Nghĩa", "Minh Hiếu", 
-         "Quốc Thái", "Hoàng Phúc", "Trung Hiếu", "Nhật Hoàng"
+         "Quốc Thái", "Hoàng Phúc", "Trung Hiếu", "Nhật Hoàng", "Thiên Bảo"
     ];
 
     console.log(sampleData);
